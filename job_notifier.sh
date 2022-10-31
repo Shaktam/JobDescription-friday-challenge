acfb4b7217306b3713ce299368ad75b883d40a7f
Shakti Thandavan <srishakti193@gmail.com>
2022-10-31 12:18:05 +0100

    added files

---
+++ job_notifier.sh
@@ -0,0 +1,10 @@
+mkdir bucketfold
+
+echo "zip files for s3"
+cd api_server
+zip ../bucketfold/api_server.zip requirements.txt
+zip -r ../bucketfold/api_server.zip source_main
+cd ..
+
+echo "upload to s3"
+aws s3 cp bucketfold/api_server.zip s3://jobnotification-s3/
